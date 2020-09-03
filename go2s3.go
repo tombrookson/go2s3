f2332c2637bad031a0a15dd150f371f751f35540
Tom Brookson <tombrookson97@gmail.com>
2020-09-03 20:15:08 +0100

    initial commit

---
+++ go2s3.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/request"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/aws/endpoints"
+)
+
+func main() {
+	var bucket, key string
+	var timeout time.Duration
+
+	flag.StringVar(&bucket, "b", "", "Bucket name.")
+	flag.StringVar(&key, "k", "", "Object key name.")
+	flag.DurationVar(&timeout, "d", 0, "Upload timeout.")
+	flag.Parse()
+
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region: aws.String(endpoints.EuWest2RegionID),
+	}))
+
+	svc := s3.New(sess)
+
+	ctx := context.Background()
+	var cancelFn func()
+	if timeout > 0 {
+		ctx, cancelFn = context.WithTimeout(ctx, timeout)
+	}
+
+	if cancelFn != nil {
+		defer cancelFn()
+	}
+
+	_, err := svc.PutObjectWithContext(ctx, &s3.PutObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+		Body:   os.Stdin,
+	})
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok && aerr.Code() == request.CanceledErrorCode {
+			fmt.Fprintf(os.Stderr, "upload cancelled due to timeout, %v\n", err)
+		} else {
+			fmt.Fprintf(os.Stderr, "failed to upload object, %v\n", err)
+		}
+		os.Exit(1)
+	}
+
+	fmt.Printf("successfully uploaded file to %s/%s\n", bucket, key)
+}
